fa0041a3940715bd4cc8803ce57071b66ed773e3
maria-eduarda-schwinn <maria.eduarda.schwinn@gmail.com>
2024-10-09 19:34:15 -0300

    add S3Configuration

---
+++ src/main/java/br/com/mariaschwinn/s3/configuration/S3Configuration.java
@@ -0,0 +1,26 @@
+package br.com.mariaschwinn.s3.configuration;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class S3Configuration {
+
+    @Bean
+    public AmazonS3 amazonS3() {
+        return AmazonS3ClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(
+                        "http://localhost:4566",
+                        "us-east-1"))
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(
+                        "localstack",
+                        "localstack")))
+                .withPathStyleAccessEnabled(true)
+                .build();
+    }
+}
