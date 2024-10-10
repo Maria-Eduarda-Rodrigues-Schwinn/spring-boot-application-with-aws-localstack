e383846bd3808cc9d2d3b7d3b1c4e20556e7ae60
maria-eduarda-schwinn <maria.eduarda.schwinn@gmail.com>
2024-10-10 18:47:32 -0300

    add aws sns

---
+++ src/main/java/br/com/mariaschwinn/sns/configuration/SNSConfiguration.java
@@ -0,0 +1,25 @@
+package br.com.mariaschwinn.sns.configuration;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.AmazonSNSClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SNSConfiguration {
+
+    @Bean
+    public AmazonSNS amazonSNS() {
+        return AmazonSNSClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(
+                        "http://localhost:4566",
+                        "us-east-1"))
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(
+                        "localstack",
+                        "localstack")))
+                .build();
+    }
+}
