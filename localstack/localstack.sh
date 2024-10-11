c97c82975cab0d353659c2c21e197a27c04f7778
maria-eduarda-schwinn <maria.eduarda.schwinn@gmail.com>
2024-10-11 19:17:39 -0300

    add postman collection

--- localstack/localstack.sh
+++ localstack/localstack.sh
@@ -17,3 +17,7 @@ aws --endpoint http://localhost:4566 --profile localstack s3 mb s3://helloworld
 aws --endpoint http://localhost:4566 --profile localstack sqs create-queue --queue-name sqsHelloWorld
 aws --endpoint http://localhost:4566 --profile localstack sqs send-message --queue-url http://localhost:4566/000000000000/sqsHelloWorld --message-body "Hello World SQS!!!" --delay-seconds 1
 aws --endpoint http://localhost:4566 --profile localstack sqs receive-message --queue-url http://localhost:4566/000000000000/sqsHelloWorld
+
+# Creating Queue(Standard) in LocalStack SNS
+aws --endpoint http://localhost:4566 --profile localstack sns create-topic --name snsHelloWorld
+aws --endpoint http://localhost:4566 --profile localstack sns subscribe --topic-arn arn:aws:sns:us-east-1:000000000000:snsHelloWorld --protocol sqs --notification-endpoint arn:aws:sqs:us-east-1:000000000000:sqsHelloWorld
